bc2535df56b01f9afa7ae582d9d843976d00f7dd
dilan-2004 <dilanbarrioscortez@gmail.com>
2025-11-10 21:41:13 -0300

    Primer commit de la primera prueba de las apis para la pagina icommerce de los vinilos

---
+++ src/main/java/com/example/demo/controller/ProductController.java
@@ -0,0 +1,66 @@
+package com.example.demo.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+import com.example.demo.model.Product;
+import com.example.demo.service.ProductService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/products")
+@Tag(name = "Product Management System")
+public class ProductController {
+    @Autowired
+    private ProductService productService;
+    @GetMapping
+    @Operation(summary = "Get all products")
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @GetMapping("/{id}")
+    @Operation(summary = "Get a product by its ID")
+    public Product getProductById(@PathVariable Long id) {
+        return productService.getProductById(id);
+    }
+
+    @PostMapping
+    @Operation(summary = "Create a new product")
+    public Product createProduct(@RequestBody Product product) {
+        return productService.saveProduct(product);
+    }
+
+    @PutMapping("/{id}")
+    @Operation(summary = "Update an existing product")
+    public Product updateProduct(@PathVariable Long id, @RequestBody Product product) {
+        Product existingProduct = productService.getProductById(id);
+        if (existingProduct == null) {
+            existingProduct.setAutor(product.getAutor());
+            existingProduct.setNombre(product.getNombre());
+            existingProduct.setPrecio(product.getPrecio());
+            existingProduct.setImg(product.getImg());
+            return productService.saveProduct(existingProduct);
+        }
+        return null;
+    }
+
+    @DeleteMapping("/{id}")
+    @Operation(summary = "Delete a product by its ID")
+    public void deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
+    }
+
+}
---
+++ src/main/java/com/example/demo/model/Product.java
@@ -0,0 +1,24 @@
+package com.example.demo.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.AllArgsConstructor;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String nombre;
+    private Integer precio;
+    private String autor;
+    private String img;
+}
