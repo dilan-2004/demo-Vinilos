45c1e3445b87a69b27bcf8119137be232d691cf1
dilan-2004 <dilanbarrioscortez@gmail.com>
2025-11-20 00:29:13 -0300

    xd

--- src/main/java/com/example/demo/repository/ArtistaRepository.java
+++ src/main/java/com/example/demo/repository/ArtistaRepository.java
@@ -1,5 +1,6 @@
 package com.example.demo.repository;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,4 +11,6 @@ import com.example.demo.model.Artista;
 @Repository
 public interface ArtistaRepository extends JpaRepository<Artista, Long> {
     Optional<Artista> findByNombre(String nombre);
+
+    List<Artista> findByGeneroId(Long generoId);
 }
